badd75895f8a3db1eaae7e06bac3939d98d9b1f4
XxxCookiexxX <50168940+XxxCookiexxX@users.noreply.github.com>
2020-11-30 05:15:20 +0530

    Update wordsearch.js

--- commands/wordsearch.js
+++ commands/wordsearch.js
@@ -2,20 +2,19 @@ const randomWord = require('random-words');
 
 
 
-var success = [];
-var fail = [];
-var words = randomWord(50);
+var success, fail, words, table;
 var rows = 7;
 var cols = 7;
 
 
+
 function randomAlpha(){
     const alphabets = [
-            'a', 'b', 'c', 'd', 'e', 'f',
-            'g', 'h', 'i', 'j', 'k', 'l',
-            'm', 'n', 'o', 'p', 'q', 'r',
-            's', 't', 'u', 'v', 'w', 'x',
-            'y', 'z'
+            'A', 'B', 'C', 'D', 'E', 'F',
+            'G', 'H', 'I', 'J', 'K', 'L',
+            'M', 'N', 'O', 'P', 'Q', 'R',
+            'S', 'T', 'U', 'V', 'W', 'X',
+            'Y', 'Z'
           ];
     return alphabets[Math.floor(Math.random()*alphabets.length)];
     }
@@ -31,12 +30,15 @@ function createTable(x = 7, y = 7){
         xt[i] = yt;
         yt = new Array(y);
     }
+    success = [];
+    fail = [];
+    words = randomWord(Math.floor((x+y/2))*3);
     return xt;
 }
 
 
 function getWord(){
-    return words.shift();
+    return words.shift().toUpperCase();
 }
 
 
@@ -223,27 +225,71 @@ function fillDown(loc, word){
 }
 
 
-var table = createTable();
 
-//console.log(possibleDirections([3,3],'anu'));
-//console.log(table);
-
-for (var i = 0; i < 15; i++){
-    addWord();
-    if(success.length === 5) break;
-    //console.log(`REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE ${i}`)
+function randomize(){
+    for(var i = 0; i < rows; i++){
+        for(var j = 0; j < cols; j++){
+            if(table[i][j] === 0){
+                table[i][j] = randomAlpha();
+            }else{}
+        }
+    }
 }
 
 
-//console.log(table);
-//console.log(success.length);
+
 
 
 module.exports = {
-    name : 'wordsearch',
+    name : 'ws',
     description : 'A game where u find words in a random matrix',
     execute(message, args){
-        message.channel.send(table);
-        message.channel.send(success);
+        if(!args[0]){
+            message.channel.send('use \`.ws -help\` for more info');
+            return;
+        }
+        if(args[0].toLowerCase() === '-start' || args[0].toLowerCase() === '-s'){
+            var rows = 7, cols = 7, gameCount;
+            table = createTable(rows, cols);
+            var end = [5,6][Math.round(Math.random())];
+            for(var i = 0; i < Math.floor((rows+cols)/2)*3; i++){
+                addWord();
+                if(success.length === end) break;
+            }
+            randomize();
+            for(var i = 0; i < rows; i++){
+               message.channel.send(`\*\*\`${table[i].join('\t')}\`\*\*`);
+            }
+        } else if(args[0].toLowerCase() === '-hard' || args[0].toLowerCase() === '-h'){
+            message.channel.send(`There is a word with ${success[Math.floor(Math.random()*success.length)].length} letters`);
+        } else if(args[0].toLowerCase() === '-normal' || args[0].toLowerCase() === '-n'){
+            message.channel.send(`A word starts with ${success[Math.floor(Math.random()*success.length)][0]}`);
+        } else if(args[0].toLowerCase() === '-easy' || args[0].toLowerCase() === '-e'){
+            var easy = success[Math.floor(Math.random()*success.length)];
+            message.channel.send(`A word starts with ${easy[0]} and ends with ${easy.slice(-1)[0]}`);
+        } else if(args[0].toLowerCase() === '-answer' || args[0].toLowerCase() === '-a'){
+            message.channel.send(`You tried well. ${success.join(', ').toLowerCase()}`);
+            success = '';
+        } else if(args[0].toLowerCase() === '-show'){
+            for(var i = 0; i < 7; i++){
+                message.channel.send(`\*\*\`${table[i].join('\t')}\`\*\*`);
+            }
+        }else if(args[0].toLowerCase() === '-left' || args[0].toLowerCase() === '-l'){
+            message.channel.send(`There are ${success.length} words left in board`);
+        } else if(args[0].toLowerCase() === '-help'){
+            message.channel.send(`Welcome to wordsearch.\nIn this game, try to find a meaningful word in these random mess.\nTo start or restart use \`.ws -start\` or \`.ws -s\`.\nIf you found the word submit using \`.ws <word>\`.\nTo get hard hint use \`.ws -hard\` or \`.ws -h\`.\nTo get normal hint use \`.ws -normal\` or \`.ws -n\`.\nTo get easy hint use \`.ws -easy\` or \`.ws -e\`.\nTo give up use \`.ws -answer\` or \`.ws -a\`.\nTo see the board again use \`.ws -show\`.\nTo see how many are left use \`.ws -left\` or \`.ws -l\`\nTo see help menu use \`.ws -help\`.\nP.S: Sometimes words that are meanigful can be treated as wrong because these are generated during randomization. There are always either 5 or 6 meaningful words.\nand sometimes there is chance for same word to repeat twice.\nIncase of error, please restart board `);
+        }
+        else{
+            gameCount = success.length-1;
+            if(success.includes(args[0].toUpperCase())){
+                message.react('👍');
+                success.splice(success.indexOf(args[0].toUpperCase()),1);
+                gameCount--;
+                if(success.length === 0){
+                    message.channel.send(`you guys completed the board! yay congrats!!`);
+                }
+            } else message.react('👎');
+        }
     }
 }
+
